0b55d8db9217db24c5a2f7b6f04f25073191f651
shaikhwa <shaikhwa@MUMWD101061.ASIAPAC.NOM>
2018-02-09 16:09:08 +0530

    Update demo

--- FitnessTracker/src/main/java/com/pluralsight/controller/GoalController.java
+++ FitnessTracker/src/main/java/com/pluralsight/controller/GoalController.java
@@ -2,6 +2,7 @@ package com.pluralsight.controller;
 
 import java.util.List;
 
+import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 
 import org.dom4j.rule.Mode;
@@ -26,9 +27,14 @@ public class GoalController {
 	private GoalService goalService;
 	
 	@RequestMapping(value = "addGoal", method = RequestMethod.GET)
-	public String addGoal(Model model) {
-		Goal goal = new Goal();
-		goal.setMinutes(10);
+	public String addGoal(Model model, HttpSession session) {
+		//Goal goal = new Goal();
+		
+		Goal goal = (Goal) session.getAttribute("goal");
+		if(goal == null){
+			goal = new Goal();
+			goal.setMinutes(10);
+		}
 		model.addAttribute("goal", goal);
 		
 		return "addGoal";
